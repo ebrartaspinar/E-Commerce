4e2f4e6e7bf49f3f5c51795053bcd722e3779325
EBRAR TAŞPINAR <s220205002@ankarabilim.edu.tr>
2026-03-10 00:59:43 +0300

    chore: initialize project skeleton with Gradle multi-module build

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+rootProject.name = "trendyol-clone"
+
+include(
+    "common-lib",
+    "services:user-service",
+    "services:product-service",
+    "services:cart-service",
+    "services:order-service",
+    "services:payment-service",
+    "services:notification-service"
+)
